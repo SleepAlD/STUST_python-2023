9d5dce94b5906f97df947ac16ced3a9589a373c2
SleepAlD <j4028113737@gmail.com>
2023-11-16 12:01:33 +0800

    rrr

--- 1116/Exercise.py
+++ 1116/Exercise.py
@@ -1,20 +1,27 @@
 import math
 
 class MyShape:
+    #建立物件
     def __init__(self, side, length, width, radius):
         self.side = side
         self.length = length
         self.width = width
         self.radius = radius
 
-    def square_area(self):
-        print(self.side*self.side)
-    def rectangle_area(self):
-        print(self.length*self.width)
-    def circular_srea(self):
-        print(self.radius*self.radius*math.pi)
+    #建立輸出格式
+    def __str__(self):
+        return f"\nSquare Area = {ms.square_area()} \nRectangle Area = {ms.rectangle_area()} \nCircluar Area = {ms.circular_area()}\n"
 
+    #計算各項面積
+    def square_area(self):
+        return self.side*self.side
+    def rectangle_area(self):
+        return self.length*self.width
+    def circular_area(self):
+        return self.radius*self.radius*math.pi
+
+#輸出
 ms = MyShape(5,2,5,10)
-ms.square_area()
-ms.rectangle_area()
-ms.circular_srea()
+print(ms)
+
+
